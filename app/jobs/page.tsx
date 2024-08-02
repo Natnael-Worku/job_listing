4af7141f40b1667ee97f6bfccad05bb17ef2c2b5
Natnael Worku <natnael.worku@a2sv.org>
2024-08-02 20:19:16 +0300

    build fixes

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -1,10 +1,8 @@
-import Header from "./components/Header";
 import Cards from "./components/Cards";
 
 const JobListPage = () => {
   return (
     <div className="lg:mt-16 lg:ml-32 lg:mr-80">
-      <Header />
       <Cards />
     </div>
   );
